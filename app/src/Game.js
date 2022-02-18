1630298bcd924264fb46ce0946cd4bdf55074503
Andy Nguyen <anguyen@optimumcs.com>
2022-02-17 19:07:02 -0600

    Basic routing and blank login and game component created

---
+++ app/src/Game.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+
+
+
+export class Game extends Component {
+
+    render(){
+      return(
+        <div>
+           <h3>Create Game UI And Logic Here</h3>
+           <h3>Create Game UI And Logic Here</h3>
+           <h3>Create Game UI And Logic Here</h3>
+           <h3>Create Game UI And Logic Here</h3>
+           <h3>Create Game UI And Logic Here</h3>
+        </div>
+       
+      )
+       
+    }
+  
+}
+
+export default Game;
+  
---
+++ app/src/Login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+
+export class Login extends Component {
+
+    render(){
+      return(
+        <h3>Create Login UI and Logic Here</h3>
+      )
+       
+    }
+  
+}
+  
