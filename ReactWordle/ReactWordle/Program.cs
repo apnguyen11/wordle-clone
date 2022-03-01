4e87932ef18368394b62c913673e5014a9d0221d
Andy Nguyen <anguyen@optimumcs.com>
2022-02-28 21:52:32 -0600

    Connect to local db

--- ReactWordle/ReactWordle/Program.cs
+++ ReactWordle/ReactWordle/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ReactWordle
 {
@@ -13,6 +14,29 @@ namespace ReactWordle
     {
         public static void Main(string[] args)
         {
+            string connectionString = @"Server=(LocalDb)\WordleDBDemo;Database=WordleDB;User Id=Test;Password=Test123;";
+
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * From users";
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        string name = dr["userName"].ToString();
+                        string password = dr["password"].ToString();
+
+                        Console.WriteLine(name + " " + password);
+                    }
+                    dr.Close();
+                }
+            }
+            Console.ReadKey();
             CreateHostBuilder(args).Build().Run();
         }
 
