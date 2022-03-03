5c8bb832584071cdb96752bdbd08a62a25dd810c
Andy Nguyen <anguyen@optimumcs.com>
2022-03-02 18:46:00 -0600

    Display db name in weather forecast

--- ReactWordle2/ReactWordle2/Controllers/WeatherForecastController.cs
+++ ReactWordle2/ReactWordle2/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ReactWordle2.Controllers
 {
@@ -6,6 +7,7 @@ namespace ReactWordle2.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        public static string firstName;
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -21,11 +23,37 @@ namespace ReactWordle2.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+  
+            string connectionString = @"Server=(LocalDb)\WordleDBDemo;Database=WordleDB;User Id=Test;Password=Test123;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * From users";
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        string name = dr["userName"].ToString();
+                        string password = dr["password"].ToString();
+                        firstName = name;
+                        Console.WriteLine(name + " " + password);
+                    }
+                    dr.Close();
+                }
+            }
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+
                 Date = DateTime.Now.AddDays(index),
+               
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = firstName
+                /*Summary = Summaries[Random.Shared.Next(Summaries.Length)]*/
             })
             .ToArray();
         }
